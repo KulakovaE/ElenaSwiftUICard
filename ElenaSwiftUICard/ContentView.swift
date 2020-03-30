1d8d84de9c573d6da3483b189290fe3d0e7bc160
Elena Kulakova <egeorgievska89@gmail.com>
2020-03-30 16:56:10 +0200

    Adding image and some text.

--- ElenaSwiftUICard/ContentView.swift
+++ ElenaSwiftUICard/ContentView.swift
@@ -10,8 +10,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color(red: 0, green: 184/255, blue: 148/255).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("SelfImage").resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 150, height: 150)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/).overlay(
+                        Circle().stroke(Color.white, lineWidth: 5)
+                )
+                Text("Elena Kulakova")
+                    .font(Font.custom("Playball", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(Font.custom("PoiretOne", size: 25))
+            }
+            
+        }
+        
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
